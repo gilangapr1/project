64471575016e6eb9792eb76a48db608dfbaa9783
gilangapr1 <gilangapria4@gmail.com>
2024-03-29 10:49:54 +0700

    first init github

---
+++ aritmatika.py
@@ -0,0 +1,18 @@
+#Luas Persegi
+print ("Menghitung Luas Persegi")
+s = int(input("masukan panjang sisi persegi:"))
+luasPersegi = s * s
+print ("Luas Persegi:  "+ str (luasPersegi))
+
+#Luas Segitiga
+print ("menghitung Luas Segitiga")
+a = int (input("masukan alas segitiga:"))
+t = int (input("masukan tinggi segitiga:"))
+luasSegitiga = 1/2 * a * t
+print ("Luas Segitiga:  "+ str(luasSegitiga))
+
+#Luas Lingkaran
+print ("Menghitung luas lingkaran")
+j = int (input("Masukan jari jari lingkaran:"))
+luasLingkaran = 3.14 * j * j
+print ("Luas Lingkaran:  "+ str (luasLingkaran))
---
+++ tugas.py
@@ -0,0 +1,5 @@
+brt = 5
+hrg = 26000
+ongkos = 3000 * 2
+hargaTelur= brt * hrg
+print (uang -(hargaTelur+ongkos))
