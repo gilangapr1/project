e5e9c78c99dc64bc65f2571af12cb3322ba6757f
gilangapr1 <gilangapria4@gmail.com>
2024-04-02 19:05:09 +0700

    tugas 1

---
+++ tugas pemrograman.py
@@ -0,0 +1,9 @@
+kelerengAldi = 150
+KelerengBudi = kelerengAldi // 15
+kelerengAnto = 2 * (kelerengAldi + KelerengBudi)
+kelerengAgung = 5 - kelerengAldi + KelerengBudi + kelerengAnto
+
+print ("kelereng Budi:" + str (KelerengBudi))
+print ("kelereng Anto:" + str (kelerengAnto))
+print ("Kelereng Agung:" + str (kelerengAgung))
+                               
